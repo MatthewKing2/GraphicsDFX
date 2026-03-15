5ddbffe9b27b5508553ad0c2a47b1bbcb05f42fd
MatthewKing2 <matthewjesseking@gmail.com>
2026-03-14 22:34:35 -0500

    Drawing a Triangle - Setup - Instance

--- testing_vulkan/src/main.cpp
+++ testing_vulkan/src/main.cpp
@@ -5,12 +5,14 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication {
 private:
     GLFWwindow* window;             // Stores pointer to the window we writing stuff to
     const uint32_t WIDTH = 800;     // Window Width
     const uint32_t HEIGHT = 600;    // Window Height
+    VkInstance instance;
 
 public:
     void run() {
@@ -31,19 +33,80 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
-    // This is the main (forever) loop of the HellowTriangleApplication (render frames)
+    void createInstance() {
+        // Optonally give extra info to the graphics driver (struct)
+        // -------------------------------------------------------------------------------
+        // This data is technically optional, but it may provide some useful 
+        // information to the driver in order to optimize our specific application 
+        // (e.g. because it uses a well-known graphics engine with certain special 
+        // behavior). This struct is called VkApplicationInfo:
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // Vulkan Instance information (struct)
+        // -------------------------------------------------------------------------------
+        // This next struct is not optional and tells the Vulkan driver which global 
+        // extensions and validation layers we want to use. Global here means that 
+        // they apply to the entire program and not a specific device, which will 
+        // become clear in the next few chapters.
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        // The next two layers specify the desired global extensions. As mentioned in 
+        // the overview chapter, Vulkan is a platform agnostic API, which means that 
+        // you need an extension to interface with the window system. GLFW has a handy 
+        // built-in function that returns the extension(s) it needs to do that which we 
+        // can pass to the struct: 
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        // The last two members of the struct determine the global validation layers to 
+        // enable. We'll talk about these more in-depth in the next chapter, so just 
+        // leave these empty for now.
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // See what extensions we have: 
+        // -------------------------------------------------------------------------------
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        std::cout << "available extensions:\n";
+        for (const auto& extension : extensions) { std::cout << '\t' << extension.extensionName << '\n'; }
+
+        // Instantiate the Vulkan Object
+        // -------------------------------------------------------------------------------
+        // As you'll see, the general pattern that object creation function parameters in Vulkan follow is:
+            // Pointer to struct with creation info
+            // Pointer to custom allocator callbacks, always nullptr in this tutorial
+            // Pointer to the variable that stores the handle to the new object
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS) { throw std::runtime_error("failed to create instance!"); }
+    }
+
+
     void mainLoop() {
+        // Forever Loop: 
+        // -----------------------------------------------------------------------------------
         while (!glfwWindowShouldClose(window)) {    // while you should not close the window: 
             glfwPollEvents();                       // polls for events, like clicking the X button
         }
     }
 
     void cleanup() {
-        glfwDestroyWindow(window);      // destory the window
-        glfwTerminate();                // turn off glfw 
+        vkDestroyInstance(instance, nullptr);   // Desotry the vulkan instance
+        glfwDestroyWindow(window);              // destory the window
+        glfwTerminate();                        // turn off glfw 
     }
 };
 
